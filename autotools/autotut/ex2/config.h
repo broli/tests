3bacf238ac36300111dbf858f073bda01d577f2d
broli <carlos.ferrabone@gmail.com>
2010-01-25 17:44:31 -0300

    more test

---
+++ autotools/autotut/ex2/config.h
@@ -0,0 +1,14 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you have the atoi function.  */
+#define HAVE_ATOI 1
+
+/* Define if you have the atol function.  */
+#define HAVE_ATOL 1
+
+/* Define if you have the strtod function.  */
+#define HAVE_STRTOD 1
